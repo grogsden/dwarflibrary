dee39a11ad0d9a42f6516b2a4b5e28d985f98bca
grogsden <brian.stempien@protonmail.com>
2021-07-01 17:17:53 -0500

    added state

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
-{
+{   
+    [SerializeField] Text textComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = "You wake up from a nap at your desk, a nagging dream slowly fading from your mind.";
     }
 
     // Update is called once per frame
